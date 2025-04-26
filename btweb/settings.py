a64342e51f22c890c940e0572c4eccd759d1bde2
CJS26 <chrsan26@bergen.org>
2025-04-26 09:57:04 -0400

    Nav fix

--- btweb/settings.py
+++ btweb/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = 'django-insecure-0judyoj!n5$ac5qfyd=(sfj8r8-0kahcwi&!%@z-pt%r$mo$^v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
