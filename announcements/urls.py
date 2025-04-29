4bb56af0af3b8bf5b0fcdb2b6c3b2298fff5270d
Jayden Argudo <jayarg26@bergen.org>
2025-04-29 12:32:20 -0400

    4/29/25
    Co-authored-by: GStormcrow <GStormcrow@users.noreply.github.com>
    Co-authored-by: kngo2026 <kngo2026@users.noreply.github.com>

---
+++ announcements/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.announcement, name='home'), 
+    path('announcement', views.announcement, name='announcement'), 
+]
--- announcements/views.py
+++ announcements/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Announcement
 
-# Create your views here.
-
-def home(request):
-    announcements = Announcement.objects.filter(is_active=True).order_by('-created_at')
-    return render(request, 'home.html', {'announcements': announcements})
+# Create your views here
+def announcement(request):
+    announcements = Announcement.objects.filter().order_by('-created_at')
+    return render(request, 'index.html', {'announcements': announcements})
